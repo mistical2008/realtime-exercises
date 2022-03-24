b6bf2ce2149cfece386200c87b6b4ff0f048408f
Evgeniy Talko <etalko@gmail.com>
2022-03-24 21:47:39 +0300

    refactor(frontend): configurable interval on filed

--- polling/exercise/frontend/polling-chat.js
+++ polling/exercise/frontend/polling-chat.js
@@ -6,8 +6,10 @@ let allChat = [];
 
 // the interval to poll at in milliseconds
 const INTERVAL = 3000;
+const INTERVAL_ON_FILED = 1000;
+const MAX_TRIES = 3;
 const BACKOFF = 5000;
-let failedTries = 0;
+let FAILED_TRIES = 0;
 
 // a submit listener on the form in the HTML
 chat.addEventListener("submit", function (e) {
@@ -49,10 +51,10 @@ async function getNewMsgs() {
 
         allChat = json.msg;
         render();
-        failedTries = 0;
+        FAILED_TRIES = 0;
     } catch (error) {
         console.error("polling error", error);
-        failedTries++;
+        FAILED_TRIES++;
     }
 }
 
@@ -69,19 +71,66 @@ const template = (user, msg) => {
 };
 
 /**
+ * @description - if failed tries greater than maxTries, then backoff
+ * @param {number} arg.failedTries - number of failed requests
+ * @param {number} arg.maxTries - number of maximum failed requests without backoff
+ * @param {number} arg.backoff - time in ms
+ * @returns {number}
+ */
+function getBackoffOnMaxTries({ failedTries, maxTries, backoff = 3000 }) {
+    return failedTries <= maxTries ? 0 : backoff;
+}
+
+function getInterval({ failedTries, interval, intervalOnFiled }) {
+    if (failedTries > 0 && failedTries <= 3) {
+        return intervalOnFiled;
+    }
+    return interval;
+}
+
+/**
+ * @description - calculate time to make next request
+ * @param {number} arg.time - time in ms
+ * @param {number} arg.interval - time in ms
+ * @param {number} arg.backoff - time in ms
+ * @param {number} arg.failedTries - number of failed requests
+ * @returns {number}
+ */
+function calculateTimeToNextReq({ time, interval, backoff, failedTries }) {
+    return time + interval + backoff * failedTries;
+}
+
+/**
+ * @description runs poll in focused window. When leaves window, then stop polling
  * @param {any} cb - callback to run on every interval
  * @param {any} INTERVAL - time in ms
+ * @returns {void}
  */
 function runPoll(cb, INTERVAL, startTime = 0) {
     let timeToMakeNextRequest = startTime;
 
     /**
+     * @description timer based on requestAnimationFrame
      * @param {any} time - time in ms passed by requestAnimationFrame (started from 0)
+     * @returns {void}
      */
     async function rafTimer(time) {
         if (timeToMakeNextRequest <= time) {
             await cb();
-            timeToMakeNextRequest = time + INTERVAL + BACKOFF * failedTries;
+            timeToMakeNextRequest = calculateTimeToNextReq({
+                time,
+                interval: getInterval({
+                    failedTries: FAILED_TRIES,
+                    interval: INTERVAL,
+                    intervalOnFiled: INTERVAL_ON_FILED,
+                }),
+                backoff: getBackoffOnMaxTries({
+                    backoff: BACKOFF,
+                    maxTries: MAX_TRIES,
+                    failedTries: FAILED_TRIES,
+                }),
+                failedTries: FAILED_TRIES,
+            });
         }
         requestAnimationFrame(rafTimer);
     }
